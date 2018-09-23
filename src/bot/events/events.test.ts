68192b914e257c3529f532c958b685d8e758726f
Florian Regensburger <rflorian@users.noreply.github.com>
2018-09-23 13:01:14 +0200

    fix(deps): updated

--- src/bot/events/events.test.ts
+++ src/bot/events/events.test.ts
@@ -1,4 +1,4 @@
-import { keepoBotEvents } from './events';
+import { keepoBotEvents } from './keepo-bot-events';
 
 describe('keepobot events', () => {
     it('is defined', () => expect(keepoBotEvents).toBeDefined());
